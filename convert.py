4df3e6ee1557c2b41891086da3c51aa134d743e1
Tom White <tom@cloudera.com>
2015-11-21 11:06:04 +0000

    Add a bit more detail to instructions.

--- convert.py
+++ convert.py
@@ -1,6 +1,6 @@
 import re
 
-print "date\tofficial_sunrise\tofficial_sunset\tcivil_sunrise\tcivil_sunset\tnautical_sunrise\tnautical_sunset\tastronomical_sunrise\tastronomical_sunset"
+print("date\tofficial_sunrise\tofficial_sunset\tcivil_sunrise\tcivil_sunset\tnautical_sunrise\tnautical_sunset\tastronomical_sunrise\tastronomical_sunset")
 
 p = re.compile(r'^(\d{2})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4})  ([0-9 /=]{4}) ([0-9 /=]{4}).*$')
 
@@ -41,8 +41,8 @@ for month in range(12):
     nautical_sunrise, nautical_sunset = nautical_times[date][2 * month], nautical_times[date][2 * month + 1]
     astronomical_sunrise, astronomical_sunset = astronomical_times[date][2 * month], astronomical_times[date][2 * month + 1]
     if official_sunrise.strip():
-      print '{0:0>2}/{1:0>2}/{2}\t{3}:{4}\t{5}:{6}\t{7}:{8}\t{9}:{10}\t{11}:{12}\t{13}:{14}\t{15}:{16}\t{17}:{18}'.format((date+1), (month+1), year%100, \
+      print('{0:0>2}/{1:0>2}/{2}\t{3}:{4}\t{5}:{6}\t{7}:{8}\t{9}:{10}\t{11}:{12}\t{13}:{14}\t{15}:{16}\t{17}:{18}'.format((date+1), (month+1), year%100, \
         official_sunrise[0:2], official_sunrise[2:4], official_sunset[0:2], official_sunset[2:4], \
         civil_sunrise[0:2], civil_sunrise[2:4], civil_sunset[0:2], civil_sunset[2:4], \
         nautical_sunrise[0:2], nautical_sunrise[2:4], nautical_sunset[0:2], nautical_sunset[2:4], \
-        astronomical_sunrise[0:2], astronomical_sunrise[2:4], astronomical_sunset[0:2], astronomical_sunset[2:4])
+        astronomical_sunrise[0:2], astronomical_sunrise[2:4], astronomical_sunset[0:2], astronomical_sunset[2:4]))
